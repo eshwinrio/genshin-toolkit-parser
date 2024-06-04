9196722ba570ece20c4016af2790d294060e2dea
Eshwin Rio <riopinto487@gmail.com>
2024-06-03 21:19:04 -0400

    Updated tests

--- dist/tests/parser.test.js
+++ dist/tests/parser.test.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -31,41 +8,62 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
-const node_test_1 = __importStar(require("node:test"));
+const node_test_1 = require("node:test");
 const parser_1 = require("../lib/parser");
+const os_1 = require("os");
+const path_1 = require("path");
+const node_assert_1 = __importDefault(require("node:assert"));
 (0, node_test_1.describe)("DataProvider tests for HAR", { concurrency: true, skip: !process.env.FS_PATH_TO_HAR }, (_suiteContext) => {
-    (0, node_test_1.default)("FileDataProvider", (_testContext) => __awaiter(void 0, void 0, void 0, function* () {
+    let temporaryDirectory;
+    (0, node_test_1.before)((_testContext) => __awaiter(void 0, void 0, void 0, function* () {
+        process.env.NODE_ENV = "test";
+        return new Promise((resolve, reject) => {
+            (0, fs_1.mkdtemp)(`${(0, os_1.tmpdir)()}${path_1.sep}`, (err, folder) => {
+                if (err)
+                    reject(err);
+                temporaryDirectory = folder;
+                resolve(true);
+            });
+        });
+    }));
+    (0, node_test_1.after)((_testContext) => __awaiter(void 0, void 0, void 0, function* () {
+        yield new Promise((resolve, reject) => {
+            (0, fs_1.rm)(temporaryDirectory, { recursive: true, force: true }, (err) => {
+                if (err)
+                    reject(err);
+                resolve(true);
+            });
+        });
+    }));
+    (0, node_test_1.it)("FileDataProvider", (_testContext) => __awaiter(void 0, void 0, void 0, function* () {
         const provider = new parser_1.FileDataProvider(process.env.FS_PATH_TO_HAR);
         const gameDataFactory = yield parser_1.GameDataFactory.loadFromHar(provider);
-        console.table(gameDataFactory.gameData.avatars.map(avatar => {
-            var _a, _b, _c;
-            return ({
-                character: avatar.name,
-                level: avatar.level,
-                weapon: `${(_a = avatar.weapon) === null || _a === void 0 ? void 0 : _a.type_name}/${(_b = avatar.weapon) === null || _b === void 0 ? void 0 : _b.name} - lv${(_c = avatar.weapon) === null || _c === void 0 ? void 0 : _c.level}`,
-                friendship: `lv${avatar.fetter}`
-            });
-        }));
+        node_assert_1.default.ok(gameDataFactory.gameData);
     }));
-    (0, node_test_1.default)("BufferDataProvider", (_testContext) => {
-        (0, fs_1.readFile)(process.env.FS_PATH_TO_HAR, (err, data) => {
-            if (err) {
-                throw err;
-            }
-            const provider = new parser_1.BufferDataProvider(data);
-            parser_1.GameDataFactory.loadFromHar(provider).then(gameDataFactory => {
-                console.table(gameDataFactory.gameData.avatars.map(avatar => {
-                    var _a, _b, _c;
-                    return ({
-                        character: avatar.name,
-                        level: avatar.level,
-                        weapon: `${(_a = avatar.weapon) === null || _a === void 0 ? void 0 : _a.type_name}/${(_b = avatar.weapon) === null || _b === void 0 ? void 0 : _b.name} - lv${(_c = avatar.weapon) === null || _c === void 0 ? void 0 : _c.level}`,
-                        friendship: `lv${avatar.fetter}`
-                    });
-                }));
+    (0, node_test_1.it)("BufferDataProvider", (_testContext) => {
+        return new Promise((resolve, reject) => {
+            (0, fs_1.readFile)(process.env.FS_PATH_TO_HAR, (err, data) => {
+                if (err)
+                    reject(err);
+                const provider = new parser_1.BufferDataProvider(data);
+                parser_1.GameDataFactory.loadFromHar(provider).then(gameDataFactory => {
+                    node_assert_1.default.ok(gameDataFactory.gameData);
+                    resolve();
+                });
             });
         });
     });
+    (0, node_test_1.it)("File output", (_testContext) => __awaiter(void 0, void 0, void 0, function* () {
+        const provider = new parser_1.FileDataProvider(process.env.FS_PATH_TO_HAR);
+        const gameDataFactory = yield parser_1.GameDataFactory.loadFromHar(provider);
+        yield gameDataFactory.saveToFile(temporaryDirectory + "output.json");
+        const provider2 = new parser_1.FileDataProvider(temporaryDirectory + "output.json");
+        const gameDataFactory2 = yield parser_1.GameDataFactory.loadFromFile(provider2);
+        node_assert_1.default.ok(gameDataFactory2.gameData);
+    }));
 });
--- src/tests/parser.test.ts
+++ src/tests/parser.test.ts
@@ -1,38 +1,63 @@
-import { readFile } from "fs";
-import test, { describe } from "node:test";
+import { readFile, mkdtemp, rm } from "fs";
+import { after, before, describe, it } from "node:test";
 import { BufferDataProvider, FileDataProvider, GameDataFactory } from "../lib/parser";
+import { tmpdir } from "os";
+import { sep } from "path";
+import assert from "node:assert";
 
 describe(
     "DataProvider tests for HAR",
     { concurrency: true, skip: !process.env.FS_PATH_TO_HAR },
     (_suiteContext) => {
 
-        test("FileDataProvider", async (_testContext) => {
-            const provider = new FileDataProvider(process.env.FS_PATH_TO_HAR!);
-            const gameDataFactory = await GameDataFactory.loadFromHar(provider);
-            console.table(gameDataFactory.gameData.avatars.map(avatar => ({
-                character: avatar.name,
-                level: avatar.level,
-                weapon: `${avatar.weapon?.type_name}/${avatar.weapon?.name} - lv${avatar.weapon?.level}`,
-                friendship: `lv${avatar.fetter}`
-            })));
+        let temporaryDirectory: string;
+
+        before(async (_testContext) => {
+            process.env.NODE_ENV = "test";
+            return new Promise((resolve, reject) => {
+                mkdtemp(`${tmpdir()}${sep}`, (err, folder) => {
+                    if (err) reject(err);
+                    temporaryDirectory = folder;
+                    resolve(true);
+                });
+            })
         });
 
-        test("BufferDataProvider", (_testContext) => {
-            readFile(process.env.FS_PATH_TO_HAR!, (err, data) => {
-                if (err) {
-                    throw err;
-                }
-                const provider = new BufferDataProvider(data);
-                GameDataFactory.loadFromHar(provider).then(gameDataFactory => {
-                    console.table(gameDataFactory.gameData.avatars.map(avatar => ({
-                        character: avatar.name,
-                        level: avatar.level,
-                        weapon: `${avatar.weapon?.type_name}/${avatar.weapon?.name} - lv${avatar.weapon?.level}`,
-                        friendship: `lv${avatar.fetter}`
-                    })));
+        after(async (_testContext) => {
+            await new Promise((resolve, reject) => {
+                rm(temporaryDirectory, { recursive: true, force: true }, (err) => {
+                    if (err) reject(err);
+                    resolve(true);
                 });
             });
+        });
+
+        it("FileDataProvider", async (_testContext) => {
+            const provider = new FileDataProvider(process.env.FS_PATH_TO_HAR!);
+            const gameDataFactory = await GameDataFactory.loadFromHar(provider);
+            assert.ok(gameDataFactory.gameData);
+        });
+
+        it("BufferDataProvider", (_testContext) => {
+            return new Promise((resolve, reject) => {
+                readFile(process.env.FS_PATH_TO_HAR!, (err, data) => {
+                    if (err) reject(err);
+                    const provider = new BufferDataProvider(data);
+                    GameDataFactory.loadFromHar(provider).then(gameDataFactory => {
+                        assert.ok(gameDataFactory.gameData);
+                        resolve();
+                    });
+                });
+            });
+        });
+
+        it("File output", async (_testContext) => {
+            const provider = new FileDataProvider(process.env.FS_PATH_TO_HAR!);
+            const gameDataFactory = await GameDataFactory.loadFromHar(provider);
+            await gameDataFactory.saveToFile(temporaryDirectory + "output.json");
+            const provider2 = new FileDataProvider(temporaryDirectory + "output.json");
+            const gameDataFactory2 = await GameDataFactory.loadFromFile(provider2);
+            assert.ok(gameDataFactory2.gameData);
         });
     }
 )
